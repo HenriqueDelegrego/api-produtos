78fc76c842c69f1f5688c1ff304ba954b48f3b14
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-09-04 17:24:19 -0300

    style

--- src/main/java/com/delegrego/api_produtos/controller/ProdutoController.java
+++ src/main/java/com/delegrego/api_produtos/controller/ProdutoController.java
@@ -38,7 +38,8 @@ public class ProdutoController {
 	}
 
 	@GetMapping
-	public ResponseEntity<List<ProdutoListResponse>> listarProdutos(@RequestParam(required = false) String nome, @RequestParam(required = false) String descricao) {
+	public ResponseEntity<List<ProdutoListResponse>> listarProdutos(@RequestParam(required = false) String nome,
+			@RequestParam(required = false) String descricao) {
 		return ResponseEntity.status(HttpStatus.OK).body(servico.listarProdutos(nome, descricao));
 	}
 
