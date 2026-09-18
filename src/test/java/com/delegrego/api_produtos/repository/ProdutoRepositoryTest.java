4e3f7d3da75831dfaaab089391571b39adaf136e
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-09-18 10:09:32 -0300

    add comments

--- src/test/java/com/delegrego/api_produtos/repository/ProdutoRepositoryTest.java
+++ src/test/java/com/delegrego/api_produtos/repository/ProdutoRepositoryTest.java
@@ -15,14 +15,18 @@ public class ProdutoRepositoryTest {
 
 	@Test
 	void Save_ValidData_ReturnsProduct() {
+
+		// Arrange
 		Produto produto = new Produto();
 		produto.setNome("Caneta");
 		produto.setDescricao("Azul");
 		produto.setPreco(2);
 		produto.setUrlImagem("a");
 
+		// Act
 		Produto saved = repository.save(produto);
 
+		// Assert
 		Assertions.assertThat(saved).isNotNull();
 		Assertions.assertThat(saved.getNome()).isEqualTo("Caneta");
 	}
