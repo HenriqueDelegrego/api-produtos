f0a929efacfab0c031fb84667d64f8174acf980d
Henrique Delegrego <62546083+HenriqueDelegrego@users.noreply.github.com>
2026-09-21 01:38:12 -0300

    add partial search test

--- src/test/java/com/delegrego/api_produtos/service/ProdutoServiceTest.java
+++ src/test/java/com/delegrego/api_produtos/service/ProdutoServiceTest.java
@@ -1,7 +1,6 @@
 package com.delegrego.api_produtos.service;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
@@ -87,6 +86,39 @@ public class ProdutoServiceTest {
 
 		// Act
 		List<ProdutoListResponse> listaProdutos = service.listarProdutos(null, null);
+
+		// Assert
+		Assertions.assertThat(listaProdutos).containsExactlyInAnyOrder(response1, response2);
+	}
+
+	@Test
+	void deveListarProdutosQuandoPesquisaParcialPorNomeForValida() {
+
+		// Arrange
+		Produto produto1 = new Produto();
+		produto1.setNome("Caneta");
+		produto1.setDescricao("Azul");
+		produto1.setPreco(new BigDecimal("2.00"));
+		produto1.setUrlImagem("a");
+
+		Produto produto2 = new Produto();
+		produto2.setNome("Canetão");
+		produto2.setDescricao("Azul");
+		produto2.setPreco(new BigDecimal("1.50"));
+		produto2.setUrlImagem("b");
+
+		ProdutoListResponse response1 = new ProdutoListResponse(produto1.getId(), produto1.getNome(),
+				produto1.getPreco(), produto1.getUrlImagem());
+
+		ProdutoListResponse response2 = new ProdutoListResponse(produto2.getId(), produto2.getNome(),
+				produto2.getPreco(), produto2.getUrlImagem());
+
+		Mockito.when(repository.findByNomeContainingIgnoreCase("Canet")).thenReturn(List.of(produto1, produto2));
+
+		Mockito.when(mapper.toListResponse(List.of(produto1, produto2))).thenReturn(List.of(response1, response2));
+
+		// Act
+		List<ProdutoListResponse> listaProdutos = service.listarProdutos("Canet", null);
 
 		// Assert
 		Assertions.assertThat(listaProdutos).containsExactlyInAnyOrder(response1, response2);
